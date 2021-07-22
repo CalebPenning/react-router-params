0eecbc5df4f99a23e09ca4e4321f3cda5c20b540
calebpenning <calebpenning@mail.com>
2021-07-22 15:04:35 -0700

    when you submit a search term, returns first movie poster of the returned data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import logo from './logo.svg'
+import Movie from './components/Movie'
 import './App.css'
+import { useState } from 'react'
 
-const App = () => (
-  <div></div>
-)
+const App = () => {
+  const [movieTitle, setMovieTitle] = useState("")
+  const [inputHidden, setInputHidden] = useState(false)
+  const [movieHidden, setMovieHidden] = useState(true)
+
+  const handleChange = e => {
+    setMovieTitle(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setInputHidden(true)
+    setMovieHidden(false)
+  }
+
+  return (
+    <div>
+    { !inputHidden && movieHidden &&
+      <form onSubmit={handleSubmit}>
+        <label>Search For A Movie:</label>
+        <input id="title" name="title" onChange={handleChange} />
+      </form>
+    }
+    {  inputHidden && !movieHidden &&
+      <Movie title={movieTitle} />
+    }
+    </div>
+  )
+}
 export default App
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,34 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
 
-const Movie = ({ title }) => {
+const OMDB_URL = "http://www.omdbapi.com/"
     
+const Movie = ({ title }) => {
+    const [src, setSrc] = useState(null)
+
+    useEffect(() => {
+        const fetchMovie = async (searchTerm) => {
+            try {
+                let res = await axios.get(`${OMDB_URL}`, {
+                    params: { apikey: "f4b4cbdc", s: searchTerm }
+                })
+                setSrc(res.data.Search[0].Poster)
+                // set src for movie poster
+            } 
+            catch(err) {
+                console.log(err)
+            }
+        }
+        fetchMovie(title)
+    }, [title])
+
+    let img = src ? <img src={src} alt={`Results for search term ${title}`} /> : null
+    return (
+        <div>
+            <h1>Results for {`${title}`}</h1>
+            {img}
+        </div>
+    )
 }
 
 export default Movie
